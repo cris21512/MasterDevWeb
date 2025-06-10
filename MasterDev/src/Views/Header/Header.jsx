8d7d8a9d22200881059f69201d64c5bb8205572a
Master21 <mastercrack2112.5@gmail.com>
2025-06-10 17:54:45 -0600

    First version

---
+++ MasterDev/src/Views/Header/Header.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import './Header.css'
+import { useTranslation } from 'react-i18next'
+import GradientText from '../../TextAnimations/GradientText/GradientText'
+import Particles from '../../Backgrounds/Particles/Particles'
+import MasterDev from '../../assets/images/MasterDev.png'
+
+const Header = () => {
+  const [t, i18n] = useTranslation("global");
+
+  return (
+    <div className='Header-style'>
+      <div
+        style={{
+          width: '100%',
+          height: '100vh',
+          position: 'absolute',
+          overflow: 'hidden',
+        }}
+      >
+        <Particles
+          particleColors={['#00eaff', '#00eaff']}
+          particleCount={250}
+          particleSpread={10}
+          speed={0.6}
+          particleBaseSize={100}
+          moveParticlesOnHover={false}
+          alphaParticles={true}
+          disableRotation={true}
+        />
+      </div>
+      <section className="Center-header">
+        <div className='text-and-buttons'>
+          <section className='text-header'>
+            <h1 className='Header-title'>{t("Header.Title")}</h1>
+            <div>
+              <GradientText
+                colors={["#40ffaa", "#4079ff", "#40ffaa", "#4079ff", "#40ffaa"]}
+                animationSpeed={3}
+                showBorder={false}
+                className="custom-class"
+              >
+                {t("Header.Subtitle")}
+              </GradientText>
+            </div>
+          </section>
+          <div className='Buttons-header'>
+            
+          </div>
+        </div>
+        <img src={MasterDev} alt="MasterDev" className='imagen-master' />
+      </section>
+    </div>
+  );
+};
+
+export default Header;
---
+++ MasterDev/src/main.jsx
@@ -0,0 +1,44 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import ReactDOM from 'react-dom/client'
+import ReactLenis from 'lenis/react'
+import 'lenis/dist/lenis.css'
+import App from './App.jsx'
+import { I18nextProvider } from 'react-i18next'
+import i18next from 'i18next'
+
+import global_es from './locales/es/global.json'
+import global_en from './locales/en/global.json'
+
+
+
+i18next.init({
+  interpolation: {escapeValue: false},
+  lng: "en", // default language
+  resources: {
+    es: {
+      global: global_es,
+    },
+    en: {
+      global: global_en,
+    },
+  },
+})
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <I18nextProvider i18n={i18next}>
+      <App />
+    <ReactLenis
+      root
+      options={{
+        duration: 0.8,
+        easing: t => 1 - Math.pow(1 - t, 3),
+        smoothTouch: true
+      }}
+    >
+    </ReactLenis>
+    </I18nextProvider>
+  </StrictMode>,
+)
